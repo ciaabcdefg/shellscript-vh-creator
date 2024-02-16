c3d9c6b219f1ff3b63e0a83cfa5293ba7c0ed03f
ciaabcdefg <ciaabcdefg@gmail.com>
2024-02-17 01:56:29 +0700

    finished. potential bugs may occur

--- vh-creator.sh
+++ vh-creator.sh
@@ -2,6 +2,12 @@
 
 # --- --- --- PREAMBLE --- --- ---
 
+YELLOW="\033[33m"
+GREEN="\033[92m"
+RED="\033[91m"
+CYAN="\033[96m"
+RESET="\033[0m"
+
 cfg_path="config.cfg"
 
 # match($1 = prefix) -> string: Matches a line with the prefix $prefix in $cfg_path and returns the succeeding string.
@@ -36,6 +42,7 @@ sample_nginx_config_path=$(match sample_nginx_config_path)
 sample_index_path=$(match sample_index_path)
 
 temp_path=$(match temp_path)
+backup_path=$(match backup_path)
 
 # clean(void) -> void: Cleans the temp directory. Immediately exits with code 1 when $temp_path is the root directory '/' or its children '/*' 
 clean() {
@@ -44,7 +51,7 @@ clean() {
 		exit 1
 	else
 		find $temp_path -mindepth 1 -delete	
-		echo "Temp directory cleaned successfully."
+		# echo "Temp directory cleaned successfully."
 	fi
 }
 
@@ -68,7 +75,7 @@ echo -n ">> Enter server name (e.g. python.cpe36.net): "
 read server_name
 server_name=$(echo $server_name | tr -d ' ')
 
-# Copy the Nginx template file to temp
+# Copy the template files to temp
 
 temp_nginx_config_path="$temp_path/$server_name"
 temp_index_path="$temp_path/index.html"
@@ -76,7 +83,7 @@ temp_index_path="$temp_path/index.html"
 cp $sample_nginx_config_path $temp_nginx_config_path
 cp $sample_index_path $temp_index_path
 
-# Replace the temp files definitions with new ones
+# Modify the template files
 
 replace "root" "$html_path/$server_name;" $temp_nginx_config_path
 replace "server_name" "$server_name;" $temp_nginx_config_path
@@ -90,13 +97,102 @@ temp_dns_reverse_path="$temp_path/$db_reverse_lookup_name"
 cp $dns_forward_lookup_path $temp_dns_forward_path
 cp $dns_reverse_lookup_path $temp_dns_reverse_path
 
+# Modify the database files
+
 line="$(get_dns_self)\tIN\tPTR\t$server_name.\t;"
 echo $line >> $temp_dns_reverse_path
 
 line="$server_name.\t\tIN\tA\t$(hostname -i)"
 echo $line >> $temp_dns_forward_path
 
+# Back up the DNS database files in case things go wrong
 
+timestamped_backup_path="$backup_path/$(date +%s)"
+mkdir $timestamped_backup_path
 
+cp $dns_forward_lookup_path "$timestamped_backup_path/$db_name"
+cp $dns_reverse_lookup_path "$timestamped_backup_path/$db_reverse_lookup_name"
 
+# Dispatch the Nginx config files to nginx/sites-available and create symbolic links in sites-enabled
+
+mv $temp_nginx_config_path "$sites_available_path/$server_name"
+ln -s "$sites_available_path/$server_name" "$sites_enabled_path/$server_name" 2>/dev/null
+
+# Create a new directory for the website and dispatch the index file to it
+
+echo
+printf $CYAN"Dispatching site configuration files..."$RESET"\n"
+mkdir "$html_path/$server_name" 2>/dev/null
+mv $temp_index_path "$html_path/$server_name"
+
+# Check config & restart Nginx
+
+printf $YELLOW"Checking Nginx config..."$RESET"\n"
+echo
+
+nginx -t
+if [ $? -eq 0 ]; then
+	echo
+    	printf $GREEN"Nginx configuration check successful."$RESET"\n"
+	echo "Restarting Nginx..."
+else
+	echo    	
+	printf $RED"ERROR: erroneous Nginx configuration. Check $sites_available_path/$server_name for errors."$RESET"\n"
+	echo "Process aborted."
+	exit 1
+fi
+
+service nginx restart
+
+# Dispatch the database files to bind/zones
+# But first, check if the database files already contain the domain names.
+
+echo
+
+# Check forward lookup file
+
+printf $CYAN"Dispatching DNS DB files..."$RESET"\n"
+if grep -q $server_name $dns_forward_lookup_path; then
+	printf "Definition for $server_name already exists in the forward lookup file.\n"
+	echo "Aborting update (none needed.)"
+else
+	mv $temp_dns_forward_path $dns_forward_lookup_path
+fi
+
+# Check reverse lookup file
+
+if grep -q $server_name $dns_reverse_lookup_path; then
+	printf "Definition for $server_name already exists in the reverse lookup file.\n"
+	echo "Aborting update (none needed.)"
+else
+	mv $temp_dns_reverse_path $dns_reverse_lookup_path
+fi
+
+# Check config & restart Named
+
+printf $YELLOW"Checking Named config..."$RESET"\n"
+echo
+
+named-checkconf
+if [ $? -eq 0 ]; then
+	printf $GREEN"Named configuration check successful."$RESET"\n"
+	echo "Restarting Named..."
+else
+	echo
+	printf $RED"ERROR: erroneous Named configuration."$RESET"\n"
+	echo "Check $dns_forward_lookup_path & $dns_reverse_lookup_path for errors."
+	printf "To perform a rollback, type: "
+	printf $YELLOW"mv $timestamped_backup_path/* $zones_path"$RESET"\n"
+	echo "Process aborted."	
+	exit 1
+fi
+
+service named restart
+
+echo
+printf $GREEN"Site created successfully, try:$RESET lynx $server_name""\n"
+echo
+echo "To revert the DNS configurations to an earlier state, type:"
+printf $YELLOW"mv $timestamped_backup_path/* $zones_path"$RESET"\n"
+echo
 
